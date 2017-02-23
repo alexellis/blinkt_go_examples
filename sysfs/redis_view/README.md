e69a2be0e4138df8aa648d7c16d3e4bffff688f4
Alex Ellis <AlexEllis2@gmail.com>
2017-02-23 20:37:32 +0000

    Add builder pattern for redis view / sender

---
+++ sysfs/redis_view/README.md
@@ -0,0 +1,48 @@
+### Synchronised Blinkt via Redis
+
+We have sender code, receiver code and then Redis as the connective tissue using a channel to exchange information.
+
+
+* Run this on your RPis that have a Blinkt attached:
+
+```
+$ docker run -v /sys:/sys -e ADDR=192.168.2.21 -d alexellis2/redis-view
+```
+
+*Alter the ADDR to match the IP of the machine with Redis*
+
+* To run Redis on an RPi:
+
+```
+$ docker run --name redis -d -p 6379:6379 alexellis2/redis-armhf
+```
+
+If you want to debug messages on Redis:
+
+```
+$ docker run -ti redis-armhf redis-cli -h 192.168.2.21
+```
+
+* Run this on your "sender"
+
+```
+$ cd redis_view
+$ go run app.go
+```
+
+#### Docker Swarm
+
+For Swarm do the following:
+
+```
+$ docker network create --driver overlay --attachable=true blinkt
+
+$ docker service create --name redis --replicas=1 --network=blinkt --publish 6379:6379 \
+  alexellis2/redis-armhf:latest
+
+$ docker service create --name redis_view --mount type=bind,source=/sys,destination=/sys \
+  -e ADDR=redis --network=blinkt \
+  --mode=global alexellis2/redis-view:latest
+
+# optionally: --constraint='node.role != manager' 
+```
---
+++ sysfs/redis_view/app.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"gopkg.in/redis.v5"
+	"os"
+)
+
+import . "github.com/alexellis/blinkt_go/sysfs"
+
+type LedColor struct {
+	Red   int `json:"r"`
+	Green int `json:"g"`
+	Blue  int `json:"b"`
+}
+
+type LedMsg struct {
+	Leds []LedColor `json:"leds"`
+}
+
+func newClient() *redis.Client {
+	addr := os.Getenv("ADDR")
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr + ":6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	// Output: PONG <nil>
+	return client
+}
+
+func main() {
+	brightness := 25
+	blinkt := NewBlinkt(brightness)
+	blinkt.SetClearOnExit(true)
+
+	blinkt.Setup()
+
+	client := newClient()
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	pubsub, err := client.Subscribe("lights")
+	if err != nil {
+		panic(err)
+	}
+	defer pubsub.Close()
+	for {
+		msg, err := pubsub.ReceiveMessage()
+		if err != nil {
+			panic(err)
+		}
+		if(msg.Channel=="lights") {
+			ledMsg := LedMsg{}
+			jsonErr := json.Unmarshal([]byte(msg.Payload), &ledMsg)
+			if jsonErr != nil {
+				fmt.Println(jsonErr)
+			}
+
+			blinkt.Clear()
+
+			var r, g, b int
+			fmt.Println("Setting LEDs")
+
+			for pixel := 0; pixel < 8; pixel++ {
+				if pixel > len(ledMsg.Leds) {
+					r = 0
+					g = 0
+					b = 0
+				} else {
+					r = ledMsg.Leds[pixel].Red
+					g = ledMsg.Leds[pixel].Green
+					b = ledMsg.Leds[pixel].Blue
+				}
+				blinkt.SetPixel(pixel, r, g, b)
+			}
+			blinkt.Show()
+		}
+	}
+}
